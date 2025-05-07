4367dd0d4c9475c1c07b2631ca033adda016cfcb
Teshy01 <113466779+Teshy01@users.noreply.github.com>
2025-05-07 00:54:59 -0400

    Delete Modules directory

--- Modules/config.py
+++
@@ -1,4 +0,0 @@
-TRAIN_ANNOTATION_FILE = "raw_data/public_training_set_release_2.0/annotations.json"
-TRAIN_IMAGES_FILE = "raw_data/public_training_set_release_2.0/images"
-VAL_ANNOTATION_FILE = "raw_data/public_validation_set_2.0/annotations.json"
-VAL_IMAGES_FILE = "raw_data/public_validation_set_2.0/images"
